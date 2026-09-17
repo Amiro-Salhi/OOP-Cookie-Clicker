31e6301b6bf681d82a3ddad3f687802e9ff1d299
TheRune12 <rubenjhvanberkel@outlook.com>
2026-09-17 11:55:54 +0200

    functional farm added

--- OOP Cookie Clicker Project/Cookie Clicker/script.js
+++ OOP Cookie Clicker Project/Cookie Clicker/script.js
@@ -1,19 +1,24 @@
 let cookies = 0;
+let CpS = 0;
 
 let grandmas = 0;
 let grandma_price = 10;
 let grandma_production = 1;
 
-let CpS = 0;
+let farms = 0;
+let farm_price = 50;
+let farm_production = 5;
 
 
 const cookiebutton = document.getElementById("cookie");
 const cookies_display = document.getElementById("cookies");
+const CpS_display = document.getElementById("CpS")
 
 const buy_grandma_button = document.getElementById("buy_grandma");
 const grandma_display = document.getElementById("grandmas");
 
-const CpS_display = document.getElementById("CpS")
+const buy_farm_button = document.getElementById("buy_farm");
+const farm_display = document.getElementById("farms");
 
 
 cookiebutton.addEventListener("click" , function() {
@@ -23,15 +28,16 @@ cookiebutton.addEventListener("click" , function() {
 );
 
 function CpS_update () {
-    CpS = grandma_production * grandmas;
+    CpS = grandma_production * grandmas + farm_production * farms;
 };
 
-function grandmas_cookies() {
+function production_cookies() {
     cookies = cookies + grandma_production * grandmas;
+    cookies = cookies + farm_production * farms;
     cookies_display.textContent = cookies + " " + "Cookies";
 }
 
-setInterval(grandmas_cookies, 1000)
+setInterval(production_cookies, 1000)
 
 
 buy_grandma_button.addEventListener("click" , function() {
@@ -47,6 +53,25 @@ buy_grandma_button.addEventListener("click" , function() {
         cookies_display.textContent = cookies + " " + "Cookies";
 
         grandma_display.textContent = "Grandma's:" + " " + grandmas;
+
+        CpS_display.textContent = "Cookies Per Second:" + " " + CpS;
+    };
+
+});
+
+buy_farm_button.addEventListener("click" , function() {
+
+    if (cookies >= farm_price) {
+        
+        cookies = cookies - farm_price;
+
+        farms = farms + 1;
+
+        CpS_update()
+
+        cookies_display.textContent = cookies + " " + "Cookies";
+
+        farm_display.textContent = "Farms:" + " " + farms;
 
         CpS_display.textContent = "Cookies Per Second:" + " " + CpS;
     };
