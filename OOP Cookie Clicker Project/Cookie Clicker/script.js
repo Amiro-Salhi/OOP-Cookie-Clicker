5cb70419e9acd9cdc376365a5b592dc90ad05f20
TheRune12 <rubenjhvanberkel@outlook.com>
2026-09-23 15:32:27 +0200

    button container added, base for the upgrades

--- OOP Cookie Clicker Project/Cookie Clicker/script.js
+++ OOP Cookie Clicker Project/Cookie Clicker/script.js
@@ -1,62 +1,8 @@
 let cookies = 0;
 let CpS = 0;
-
-let grandmas = 0;
-let grandma_price = 10;
-let grandma_production = 1;
-
-let farms = 0;
-let farm_price = 50;
-let farm_production = 5;
-
-let mines = 0;
-let mine_price = 150;
-let mine_production = 15;
-
-let factorys = 0;
-let factory_price = 350;
-let factory_production = 30;
-
-let banks = 0;
-let bank_price = 550;
-let bank_production = 55;
-
-let temples = 0;
-let temple_price = 950;
-let temple_production = 90;
-
-let wizards = 0;
-let wizard_price = 1150;
-let wizard_production = 125;
-
-
 const cookiebutton = document.getElementById("cookie");
 const cookies_display = document.getElementById("cookies");
 const CpS_display = document.getElementById("CpS")
-
-const buy_grandma_button = document.getElementById("buy_grandma");
-const grandma_display = document.getElementById("grandmas");
-
-const buy_farm_button = document.getElementById("buy_farm");
-const farm_display = document.getElementById("farms");
-
-const buy_mine_button = document.getElementById("buy_mine");
-const mine_display = document.getElementById("mines");
-
-const buy_factory_button = document.getElementById("buy_factory");
-const factory_display = document.getElementById("factorys");
-
-const buy_bank_button = document.getElementById("buy_bank");
-const bank_display = document.getElementById("banks");
-
-const buy_temple_button = document.getElementById("buy_temple");
-const temple_display = document.getElementById("temples");
-
-const buy_wizard_button = document.getElementById("buy_wizard");
-const wizzard_display = document.getElementById("wizards");
-
-
-
 cookiebutton.addEventListener("click" , function() {
         cookies = cookies + 1;
         cookies_display.textContent = cookies + " " + "Cookies";
@@ -109,12 +55,42 @@ class production_unit {
             cookies_display.textContent = cookies + " " + "Cookies";
             this.display.textContent = this.name + " " + this.amount;
             
-            CpS_update ()
+            CpS_update()
             CpS_display.textContent = "Cookies Per Second:" + " " + CpS;
         }
     }
     getProductionPerSecond() {
-        return this.amount * this.production
+        return this.amount * this.production;
+    }
+}
+class base_upgrade {
+    constructor(name, price, multiplier,display_id , button_id) {
+        this.name = name;
+        this.price = price;
+        this.multiplier = multiplier;
+        this.amount = 0;
+
+        this.display = document.getElementById(display_id);
+        this.button = document.getElementById(button_id);
+
+        this.button.addEventListener("click" , () => {
+            this.buy();
+        });
+    }
+    buy() {
+        if (cookies >= this.price) {
+            cookies = cookies - this.price;
+            this.amount = this.amount + 1;
+
+            cookies_display.textContent = cookies + " " + "Cookies";
+            this.display.textContent = this.name + " " + this.amount;
+
+            CpS_update()
+            CpS_display.textContent = "Cookies Per Second" + " " + CpS;
+        }
+    }
+    getMultplier() {
+        return this.amount * this.multiplier;
     }
 }
 const grandma = new production_unit (
@@ -139,7 +115,7 @@ const mine = new production_unit (
     "buy_mine"
 )
 const factory = new production_unit (
-    "Factorys:",
+    "Factory's:",
     350,
     30,
     "factorys",
@@ -160,7 +136,7 @@ const temple = new production_unit (
     "buy_temple"
 )
 const wizard_tower = new production_unit (
-    "wizard Towers:",
+    "Wizard Towers:",
     1150,
     125,
     "wizardtowers",
